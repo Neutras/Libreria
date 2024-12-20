d261fa897a899e69acf2cd16d0a8506d617d8073
Byron Estay <109626113+Neutras@users.noreply.github.com>
2024-12-20 01:48:31 -0300

    Update authService.js

--- ecommerce-frontend/src/services/authService.js
+++ ecommerce-frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://libreria-1-6ifo.onrender.com/api";
+const API_URL = "https://libreria-1-6ifo.onrender.com/api/users";
 
 class AuthService {
   // Login
